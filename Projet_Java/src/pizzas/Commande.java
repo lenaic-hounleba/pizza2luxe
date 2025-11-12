752fcdb68506954970ca1868faeafeaf99e05b0d
Lenaïc Love Hounleba <lovehounleba@gmail.com>
2025-11-12 10:10:11 +0100

    Classe Commande

--- Projet_Java/src/pizzas/Commande.java
+++ Projet_Java/src/pizzas/Commande.java
@@ -1,6 +1,69 @@
 package pizzas;
 
-// A compléter
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Classe qui permet de créer une commande à partir des pizzas existantes et
+ * permet de gérer également l'état des commandes.
+ *
+ * @author Lenaïc Love HOUNLEBA
+ */
 public class Commande {
+  private String pizzas;
+  private double montantTotal;
+  private String adresseLivraison;
+  private List<String> etat = new ArrayList<>();
+  
+  
+  
+  public Commande(String pizzas, double montantTotal, String adresseLivraison,
+      List<String> etat) {
+    this.pizzas = pizzas;
+    this.montantTotal = montantTotal;
+    this.adresseLivraison = adresseLivraison;
+    this.etat = etat;
+  }
+  
+  
+  public String getPizzas() {
+    return pizzas;
+  }
+  
+  
+  public void setPizzas(String pizzas) {
+    this.pizzas = pizzas;
+  }
+  
+  
+  public double getmontantTotal() {
+    return montantTotal;
+  }
+  
+  
+  public void setmontantTotal(double montantTotal) {
+    this.montantTotal = montantTotal;
+  }
+  
+  
+  public void setadresseLivraison(String adresseLivraison) {
+    this.adresseLivraison = adresseLivraison;
+  }
+  
+  public String getadresseLivraison() {
+    return adresseLivraison;
+  }
+  
+  
+  public List<String> getetat() {
+    return etat;
+  }
+  
+  
+  public void setetat(List<String> etat) {
+    this.etat = etat;
+  }
+  
+  
   
 }

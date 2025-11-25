4e1e9736fc73d521958696430eba8c342246734f
Lenaïc Love Hounleba <lovehounleba@gmail.com>
2025-11-25 15:11:31 +0000

    Upload New File

---
+++ Projet_Java/src/ui/ClientControleur.java
@@ -0,0 +1,190 @@
+package ui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
+
+/**
+ * Controleur JavaFX de la fenêtre du client.
+ *
+ * @author Eric Cariou
+ */
+public class ClientControleur {
+  
+  @FXML
+  private ChoiceBox<String> choiceBoxFiltreType;
+  
+  @FXML
+  private TextField entreeAdresseClient;
+  
+  @FXML
+  private TextField entreeAgeClient;
+  
+  @FXML
+  private TextField entreeAuteurEvaluation;
+  
+  @FXML
+  private TextField entreeEmailClient;
+  
+  @FXML
+  private TextField entreeEvaluationMoyenneEvaluations;
+  
+  @FXML
+  private TextField entreeFiltreContientIngredient;
+  
+  @FXML
+  private TextField entreeFiltrePrixMax;
+  
+  @FXML
+  private TextField entreeMotDePasseClient;
+  
+  @FXML
+  private TextField entreeNomClient;
+  
+  @FXML
+  private TextField entreeNomPizza;
+  
+  @FXML
+  private TextField entreeNomPizzaEvaluee;
+  
+  @FXML
+  private TextField entreeNoteMoyennePizza;
+  
+  @FXML
+  private TextField entreePrenomClient;
+  
+  @FXML
+  private TextField entreePrixPizza;
+  
+  @FXML
+  private TextField entreeTypePizza;
+  
+  @FXML
+  private Label labelListeCommandes;
+  
+  @FXML
+  private Label labelListePizzas;
+  
+  @FXML
+  private ListView<String> listeCommandes;
+  
+  @FXML
+  private ListView<String> listeEvaluations;
+  
+  @FXML
+  private ListView<String> listeIngredients;
+  
+  @FXML
+  private ChoiceBox<Integer> choiceBoxNoteEvaluation;
+  
+  @FXML
+  private ListView<String> listePizzas;
+  
+  @FXML
+  private StackPane panePhotoPizza;
+  
+  @FXML
+  private TextArea texteCommentaireEvaluation;
+  
+  @FXML
+  void actionBoutonAfficherCommandesEnCours(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionBoutonAfficherCommandesTtraitees(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionBoutonAfficherEvaluationPizzas(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionBoutonAfficherToutesPizzas(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionBoutonAjouterMonEvaluation(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionBoutonAjouterPizzaSelectionneeCommande(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionBoutonAppliquerFiltreContientngredient(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionBoutonAppliquerFiltrePrixMax(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionBoutonAppliquerFiltreType(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionBoutonConnexion(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionBoutonCreerNouvelleCommande(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionBoutonDeconnexion(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionBoutonInscription(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionBoutonReinitialiserFiltre(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionBoutonValiderCommandeEnCours(ActionEvent event) {
+    
+  }
+  
+  @FXML
+  void actionSelectionCommnade(MouseEvent event) {
+    
+  }
+  
+  @FXML
+  void actionSelectionEvaluation(MouseEvent event) {
+    
+  }
+  
+  @FXML
+  void actionSelectionPizza(MouseEvent event) {
+    
+  }
+  
+  @FXML
+  void initialize() {
+    // Mettre ici le code d'initialisation du contenu de la fenêtre
+  }
+  
+}

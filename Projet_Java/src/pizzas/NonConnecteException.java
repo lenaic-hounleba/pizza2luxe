9524d513f4b0f04ce7cb4d238e6dc05def5aba89
Lenaïc Love Hounleba <lovehounleba@gmail.com>
2026-01-08 00:21:12 +0100

    Adaptation des méthodes de Pizza à Commande, Client. Ainsi que les méthodes qui prennaient Pizza en param

--- Projet_Java/src/pizzas/NonConnecteException.java
+++ Projet_Java/src/pizzas/NonConnecteException.java
@@ -7,12 +7,26 @@ package pizzas;
  * @author Eric Cariou
  */
 public class NonConnecteException extends Exception {
-
+  
   /**
    * Identifiant de sérialisation.
    */
   private static final long serialVersionUID = -2876441299971092712L;
   
-  // A COMPLETER SI BESOIN
+  /**
+   * Crée une exception indiquant qu'aucun utilisateur n'est connecté.
+   */
+  public NonConnecteException() {
+    super("Aucun utilisateur n'est actuellement connecté.");
+  }
+  
+  /**
+   * Crée une exception avec un message personnalisé.
+   *
+   * @param message message décrivant le problème
+   */
+  public NonConnecteException(String message) {
+    super(message);
+  }
   
 }

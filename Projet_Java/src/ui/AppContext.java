a9d95ca7f07d12ea0fbbe7bd2b10e99ec2c500e0
DorianFleurquin <dorianfleurquinsoftware@gmail.com>
2026-01-09 18:20:20 +0100

    app conxt checkstyle fixes

--- Projet_Java/src/ui/AppContext.java
+++ Projet_Java/src/ui/AppContext.java
@@ -6,50 +6,49 @@ import pizzas.GestionClient;
 import pizzas.GestionPizzaiolo;
 import pizzas.PizzeriaData;
 
-
 /**
  * Classe représentant le contexte global de l'application.
- * 
- * <p>Elle centralise l'accès aux données de la pizzeria ainsi qu'aux services
+ *
+ * <p>
+ * Elle centralise l'accès aux données de la pizzeria ainsi qu'aux services
  * métiers associés (gestion des clients et des pizzaiolos). Elle est également
  * responsable de la sauvegarde et du chargement des données à partir d'un
  * fichier unique.
  * </p>
  *
- * <p>Cette classe est conçue pour être utilisée comme point d'entrée partagé entre
+ * <p>
+ * Cette classe est conçue pour être utilisée comme point d'entrée partagé entre
  * les différentes parties de l'interface utilisateur.
  * </p>
- * 
- * 
- * @author dorianfleurquin
+ *
+ * @author Dorian Fleurquin
  */
-
 public final class AppContext {
-  
+
   private static final String SAVE_FILE = "pizzeria.dat";
-  
+
   public PizzeriaData data;
   public GestionClient clientService;
   public GestionPizzaiolo pizzaioloService;
   private final SauvegardePizzeria sauvegarde;
-  
+
+  /** Initialise les données de l'interface. */
   public AppContext() {
     this.data = new PizzeriaData();
     this.clientService = new GestionClient(data);
     this.pizzaioloService = new GestionPizzaiolo(data);
     this.sauvegarde = new SauvegardePizzeria(data);
   }
-  
+
   /** Sauvegarde les données de l'interface dans le fichier unique. */
   public void save() throws IOException {
     sauvegarde.sauvegarderDonnees(SAVE_FILE);
   }
-  
-  /** Recharge depuis le fichier unique. */
+
+  /** Recharge les données depuis le fichier unique. */
   public void load() throws IOException {
     sauvegarde.chargerDonnees(SAVE_FILE);
-    
-    
+
     this.data = sauvegarde.getData();
     this.clientService = new GestionClient(data);
     this.pizzaioloService = new GestionPizzaiolo(data);
